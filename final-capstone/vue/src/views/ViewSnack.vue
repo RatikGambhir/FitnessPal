f4b98a7271290046ac17e16cf8019146bbdc95e1
Ratik Gambhir <ratikgambhir@yahoo.com>
2021-06-04 15:58:55 -0400

    Submitting final project

---
+++ final-capstone/vue/src/views/ViewSnack.vue
@@ -0,0 +1,15 @@
+<template>
+  <view-snack />
+</template>
+
+<script>
+import ViewSnack from "../components/ViewSnack";
+export default {
+  components: {
+    ViewSnack,
+  },
+};
+</script>
+
+<style>
+</style>
